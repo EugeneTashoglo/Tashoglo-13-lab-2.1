04ecc6da2687358ad393ea4412999caca7dea3d3
user5 <user5@09.ru>
2021-10-13 13:43:24 +0200

    Добавьте файлы проекта.

---
+++ Tashoglo 13 lab 2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tashoglo_13_lab_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координату y:");
+            double y = double.Parse(Console.ReadLine());
+            if (y == x * x - 2 && y <= -x + 2 && y >= x)Console.WriteLine("Точка попадает в область");
+            else Console.WriteLine("Точка не попадает в область");
+
+
+
+
+
+
+         
+        }
+    }
+}
